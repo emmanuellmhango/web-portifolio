667591013ad20335ff9a19780fbb48655103a525
Emmanuel <emmanuellmhango@gmail.com>
2022-10-19 19:21:29 +0400

    update app.js

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@ const menuClick = document.getElementById('mobile-menu');
 const menuClose = document.querySelector('.closebtnx');
 
 menuClick.addEventListener('click', () => {
-  document.getElementById("myNav").style.width = "100%";
+  document.getElementById('myNav').style.width = '100%';
   document.getElementById('body').style.overflow = 'hidden';
   menuClick.style.display = 'none';
 });
 
 menuClose.addEventListener('click', () => {
-  document.getElementById("myNav").style.width = "0%";
+  document.getElementById('myNav').style.width = '0%';
   menuClick.style.display = 'block';
   document.getElementById('body').style.overflow = 'visible';
 });
